31ad28d3b115868c3c9fd00b270c639e62311121
River McCubbin <mccubbinri@gmail.com>
2023-06-23 11:05:46 -0400

    organized and updated js

---
+++ js/topnav.js
@@ -0,0 +1,7 @@
+function goHome() {
+    var contentDivs = document.getElementsByClassName('content')[0].getElementsByTagName('div');
+    for (var i = 0; i < contentDivs.length; i++) {
+        contentDivs[i].style.display = 'none';
+    }
+    document.getElementsById('tree').style.display = "block";
+}
