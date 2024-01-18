93ba19abaaea781056b829b0fb6a535897dbe40b
River McCubbin <mccubbinri@gmail.com>
2024-01-17 20:03:25 -0500

    removed wikimedia since github doesn't support dynamic pages, will instead be loading data from text files for each article

---
+++ js/searchbar.js
@@ -0,0 +1,9 @@
+function searchbarSearch(element) {
+    if (element.keyCode === 13) {
+        search(element);
+    }
+}
+
+function search(element) {
+    // TODO find page with the closest title to element.value and go there
+}
